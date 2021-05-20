48e1a1e339c74ab9f891e20a4150da86c0f20849
Piyush <dhawanpiyu2301@gmail.com>
2021-05-21 01:41:21 +0530

    Assignment-2 done

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,52 +1,94 @@
-          
-        var list = [
-            { "col_1": "val_11", "col_3": "val_13" },
-            { "col_2": "val_22", "col_3": "val_23" },
-            { "col_1": "val_31", "col_3": "val_33" },
-            { "col_1": "val_31","col_2": "Piyush", "col_3": "val_33" },
-        ];
-                     
-        function constructTable(selector) {
-              
-            // Getting the all column names
-            var cols = Headers(list, selector);  
-   
-            // Traversing the JSON data
-            for (var i = 0; i < list.length; i++) {
-                var row = $('<tr/>');   
-                for (var colIndex = 0; colIndex < cols.length; colIndex++)
-                {
-                    var val = list[i][cols[colIndex]];
-                      
-                    // If there is any key, which is matching
-                    // with the column name
-                    if (val == null) val = "";  
-                        row.append($('<td/>').html(val));
-                }
-                  
-                // Adding each row to the table
-                $(selector).append(row);
-            }
+var list = [
+    {
+      sno: null,
+      fname: "ashish",
+      lName: "basak",
+      city: "delhi",
+      country: "india",
+    },
+  ];
+  var cols = [];
+  
+  window.onload = function () {
+    //starting point
+    constructTable("#table");
+  };
+  
+  function constructTable(selector) {
+    // Getting the all column names
+    cols = getHeaders(list, selector);
+  
+    // Traversing the JSON data
+    for (var i = 0; i < list.length; i++) {
+      var row = $("<tr/>"); //adding each row
+      for (var colIndex = 0; colIndex < cols.length; colIndex++) {
+        var val = colIndex == 0 ? i + 1 : list[i][cols[colIndex]];
+  
+        // If there is any key, which is matching
+        // with the column name
+        if (val == null) val = "";
+        row.append($("<td/>").html(val));
+      }
+  
+      // Adding each row to the table
+      $(selector).append(row);
+    }
+  }
+  
+  function getHeaders(list, selector) {
+    var columns = [];
+    var header = $("<tr/>");
+  
+    for (var i = 0; i < list.length; i++) {
+      var row = list[i];
+  
+      for (var heading in row) {
+        //checking if heading already in columns
+        if ($.inArray(heading, columns) == -1) {
+          columns.push(heading);
+  
+          // Creating the header bu appending all headings
+          header.append($("<th/>").html(heading));
         }
-          
-        function Headers(list, selector) {
-            var columns = [];
-            var header = $('<tr/>');
-              
-            for (var i = 0; i < list.length; i++) {
-                var row = list[i];
-                  
-                for (var k in row) {
-                    if ($.inArray(k, columns) == -1) {
-                        columns.push(k);
-                          
-                        // Creating the header
-                        header.append($('<th/>').html(k));
-                    }
-                }
-            }
-              
-            // Appending the header to the table
-            $(selector).append(header);
-                return columns;
-        } 
+      }
+    }
+  
+    // Appending the header to the table
+    $(selector).append(header);
+    return columns;
+  }
+  
+  function addRowToBottom(selector) {
+    list.push({
+      sno: null,
+      fname: "piyush",
+      lName: "dhawan",
+      city: "tokyo",
+      country: "japan",
+    });
+    $(selector).empty();
+    constructTable(selector);
+  }
+  
+  function addRowToTop(selector) {
+    var newList = [];
+    newList.push({
+      sno: null,
+      fname: "piyush",
+      lName: "dhawan",
+      city: "tokyo",
+      country: "japan",
+    });
+  
+    list.forEach((row) => newList.push(row));
+    list = newList;
+    $(selector).empty();
+    constructTable(selector);
+  }
+  
+  
+  
+  
+  
+  
+  
--- assignment-2/script.js
+++ assignment-2/script.js
@@ -1,4 +1,15 @@
-function findLetter () {}
+// Function to find whether letter is present in the sentence or not.
+function findLetter (sentenceSelector,letterSelector) {
+    var sentenceValue = document.getElementById(sentenceSelector).value;
+    var letterValue = document.getElementById(letterSelector).value;
+    var index = sentenceValue.indexOf(letterValue);
+    if (index == -1) {
+        document.getElementById("output").innerHTML = "Result: The letter does not exist in the sentence";
+    }
+    else{
+       document.getElementById("output").innerHTML = "Result: "+sentenceValue.slice(index+1);
+    }
+}
 
 function displayString () {}
 
